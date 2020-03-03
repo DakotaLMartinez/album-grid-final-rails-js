7a494c9e323ec5312e51ac54eede217c89317d78
DakotaLMartinez <dakotaleemusic@gmail.com>
2020-03-03 14:37:24 -0800

    modify album_info route

--- album-api/config/routes.rb
+++ album-api/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope '/lastfm' do 
     get '/albums/:method', to: 'last_fm_search#albums'
-    get '/albuminfo/:id', to: 'last_fm_search#album_info'
+    get '/album_info/:id', to: 'last_fm_search#album_info'
   end
 end
